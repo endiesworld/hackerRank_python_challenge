89924341fef808a55876f22fb314adf8ead44f9d
Emmanuel <endiesworld@gmail.com>
2021-12-04 14:17:11 +0100

    feat: Use list comprehension

--- december/list_comprehension.py
+++ december/list_comprehension.py
@@ -3,21 +3,18 @@ def lis_comprehension():
     y = 1
     z = 2
     n = 3
-    general_lst = []
-    # n_list = []
-    for i in list(range(x+1)):
-        for j in list(range(y+1)):
-            for k in list(range(z+1)):
-                general_lst.append([i, j, k])
-    # for i in general_lst:
-    #     if((i[0] + i[1] + i[2] != n)):
-    #         n_list.append(i)
-    n_list = [x for x in general_lst if sum(x) != n]
-    print(n_list)
+    general_lst = [[i, j, k]
+                   for i in range(x+1) for j in range(y+1) for k in range(z+1) if i + j + k != n]
+    print(general_lst)
 
 
 lis_comprehension()
-# the_list = []
-# for i in list(range(2)):
-#     the_list.append(i)
-# print(the_list)
+# general_lst = []
+# n_list = []
+# for i in list(range(x+1)):
+#     for j in list(range(y+1)):
+#         for k in list(range(z+1)):
+#             general_lst.append([i, j, k])
+# for i in general_lst:
+#     if((i[0] + i[1] + i[2] != n)):
+#         n_list.append(i)
