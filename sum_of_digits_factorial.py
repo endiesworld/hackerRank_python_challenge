fb10a23a346161c3b419cd456557118961339fb9
endiesworld <endiesworld@gmail.com>
2021-12-14 11:42:04 +0100

    second update on sum of digit

--- sum_of_digits_factorial.py
+++ sum_of_digits_factorial.py
@@ -4,16 +4,27 @@ def factorial(n):
     return n * factorial(n - 1)
 
 
-def sum_of_factorial(digits):
-    sum_of_fac = 0
-    for i in digits:
-        sum_of_fac += factorial(int(i))
-    return sum_of_fac
+def f(n):
+    f_n = 0
+    str_digit = str(n)
+    for i in str_digit:
+        f_n += factorial(int(i))
+    return f_n
+
+
+def sf(n):
+    n_str = str(f(n))
+    s_f_n = 0
+    for i in n_str:
+        s_f_n += int(i)
+    return s_f_n
 
 
 def g(i):
-    f_list = [{n: factorial(n)} for n in range(1, i + 1)]
+    f_list = [{n: sf(n)} for n in range(1, i + 1)]
     return f_list
 
 
-print(g(5))
+print(sf(12345))
+print(sf(234))
+# print(sf(25))
