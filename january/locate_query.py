33f62e8be5cdb6f5b173a0c5d280ca18c6a98605
Emmanuel <endiesworld@gmail.com>
2022-01-16 06:03:49 +0100

    feat: Update on Binary Search

--- january/locate_query.py
+++ january/locate_query.py
@@ -82,6 +82,28 @@ print(loacate_query(**tests[0]["input"]) == tests[0]["output"])
 # 2.2 Complexity:
 
 
+# def locate_card(cards, query):
+#     lo, hi = 0, len(cards) - 1
+
+#     while lo <= hi:
+#         mid = (lo + hi) // 2
+#         mid_number = cards[mid]
+
+#         print("lo:", lo, ", hi:", hi, ", mid:", mid, ", mid_number:", mid_number)
+
+#         if mid_number == query:
+#             return mid
+#         elif mid_number < query:
+#             hi = mid - 1
+#         elif mid_number > query:
+#             lo = mid + 1
+
+#     return -1
+
+
+# Binary Search update: To return first index for a search
+
+
 def test_location(cards, query, mid):
     mid_number = cards[mid]
     print("mid:", mid, ", mid_number:", mid_number)
